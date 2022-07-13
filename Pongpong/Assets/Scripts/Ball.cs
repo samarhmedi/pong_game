51ed650643cf968b98f38b3f182d819788dd16b7
samarhmedi <samarhmedi@outlook.com>
2022-07-13 21:34:13 +0100

    init

---
+++ Pongpong/Assets/Scripts/Ball.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+    public Text scorerightText;
+    public Text scoreleftText;
+    int scoreRight = 0;
+    int scoreLeft = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketHeight)
+    {
+        // ascii art:
+        // ||  1 <- at the top of the racket
+        // ||
+        // ||  0 <- at the middle of the racket
+        // ||
+        // || -1 <- at the bottom of the racket
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+
+        // Note: 'col' holds the collision information. If the
+        // Ball collided with a racket, then:
+        //   col.gameObject is the racket
+        //   col.transform.position is the racket's position
+        //   col.collider is the racket's collider
+
+        // Hit the left Racket?
+        if (col.gameObject.name == "RacketLeft")
+        {
+            // Calculate hit Factor
+            float y = hitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(1, y).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        // Hit the right Racket?
+        if (col.gameObject.name == "RacketRight")
+        {
+            // Calculate hit Factor
+            float y = hitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(-1, y).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        if (col.gameObject.name == "Wallright")
+        {
+            //this line will just add 1 point to the score
+            scoreLeft++;
+            //this line will convert the int score variable to a string variable
+            scoreleftText.text = scoreLeft.ToString();
+            scoreleftText.fontSize += 30;
+            scoreleftText.color = Random.ColorHSV();
+
+        }
+        if (col.gameObject.name == "Wallleft")
+        {
+            scoreRight++;
+            scorerightText.text = scoreRight.ToString();
+            scorerightText.fontSize += 30;
+            scorerightText.color = Random.ColorHSV();
+        }
+    }
+}
---
+++ Pongpong/Assets/Scripts/FirstScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstScript : MonoBehaviour
+{
+    public Text myText;
+    void Start()
+    {
+        myText.text = "Hello World!";
+    }
+}
